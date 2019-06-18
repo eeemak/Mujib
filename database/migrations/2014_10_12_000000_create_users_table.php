c7b5389954cc5d76d4e3a627c052f9619c6e74c1
Hasan50 <tapantor24@gmail.com>
2019-06-18 16:01:15 +0600

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('fullname');
             $table->string('username');
+            $table->integer('districts_id');
+            $table->integer('thanas_id');
+            $table->integer('policeStations_id');
+            $table->string('parmanentAddress')->nullable();
+            $table->string('presentAddress')->nullable();
+            $table->string('aboutSelf')->nullable();
+            $table->string('photoPath')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreign('districts_id')->references('id')->on('districts')->onDelete('cascade');
+            $table->foreign('thanas_id')->references('id')->on('thanas')->onDelete('cascade');
+            $table->foreign('policeStations_id')->references('id')->on('police_stations')->onDelete('cascade');
+            $table->string('addedFromIp')->nullable();
             $table->timestamps();
         });
     }
