82528c2090801860e4d927b91223ac227f8b8edc
Hasan50 <tapantor24@gmail.com>
2019-06-25 16:39:05 +0600

    Update AdminUploadController.php

--- app/Http/Controllers/Admin/AdminUploadController.php
+++ app/Http/Controllers/Admin/AdminUploadController.php
@@ -32,15 +32,16 @@ class AdminUploadController extends Controller
   public function UploadFile(Request $request) {
     if ($request->file) {
       $file = $request->file;
+      $uploadob= $data = json_decode($request->title,true);
       $photo_directory = 'upload/file/';
       $photo_path = $photo_directory . time() . "-" . $file->getClientOriginalName();
       $file->move($photo_directory, $photo_path);
       // $photo_path = Storage::putFile('upload/gallary', $request->file('file'));
       $uploadfile = new FileUpload();
-      $uploadfile->file_title = $request->file_title;
+      $uploadfile->file_title = $uploadob.file_title;
       $uploadfile->file_path = $photo_path;
       $uploadfile->user_id = Auth::id();
-      $uploadfile->fileTypeId = $request->fileTypeId;
+      $uploadfile->fileTypeId =$uploadob.fileTypeId;
       $uploadfile->save();
       return true;
     }
